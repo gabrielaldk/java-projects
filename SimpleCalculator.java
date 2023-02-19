b7894f9a9dcb5ee367f50ba607ab330d9a47a5ea
Gabriela <116114874+gabrielaldk@users.noreply.github.com>
2023-02-19 12:55:14 +0100

    Update SimpleCalculator.java

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -16,6 +16,5 @@ public class SimpleCalculator {
         System.out.println(first + " - " + second + " = " + (first - second));
         System.out.println(first + " * " + second + " = " + (first * second));
         System.out.println(first + " / " + second + " = " + (1.0 * first / second));
-
     }
 }
